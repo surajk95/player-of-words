df4194af311f43d057b6dd1094824a6eb11b09cb
Suraj <surajkukati95@gmail.com>
2019-12-05 19:20:40 +0530

    Updates

---
+++ precache-manifest.dc47928a7b395191f5876d62613d48cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee49c071dd7122c639aa02aa82c3a6d",
+    "url": "/word-play/index.html"
+  },
+  {
+    "revision": "bc507ca2721869579b1c",
+    "url": "/word-play/static/css/main.b97c52c7.chunk.css"
+  },
+  {
+    "revision": "90c87a4517817139870c",
+    "url": "/word-play/static/js/2.8a2d1f71.chunk.js"
+  },
+  {
+    "revision": "bc507ca2721869579b1c",
+    "url": "/word-play/static/js/main.e09c7a3e.chunk.js"
+  },
+  {
+    "revision": "b51771235590fbe58c72",
+    "url": "/word-play/static/js/runtime-main.26fe19f0.js"
+  }
+]);
